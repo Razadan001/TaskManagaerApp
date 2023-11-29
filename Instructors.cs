7f07814b29fb3f490d0f2b7a601af8d205101a85
Ahmadraza Danmole <razadanmole@gmail.com>
2023-11-29 17:03:26 +0100

    Instructor update

---
+++ Instructors.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagaerApp
+{
+    public class Instructors : IDetails
+    {
+        public void Address(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Contact(double contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Email(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Name(string firstName, string middleName, string LastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post(string postName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
