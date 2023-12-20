1a1159f033e33d58b5c7d2c339e5c6d0f524a9a2
Ahmadraza Danmole <razadanmole@gmail.com>
2023-12-20 17:48:28 +0100

    Project

--- Details.cs
+++ Details.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 
 namespace TaskManagaerApp
 {
-<<<<<<< HEAD
     public class Details : IDetails
     {
         public void AddTask(string name)
@@ -19,33 +18,5 @@ namespace TaskManagaerApp
 
         public string? Email { get; set; }
         public string? StudentsNames {get; set; }
-=======
-    abstract class Details : IDetails
-    {
-        public void Address(string address)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Contact(double contact)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Email(string email)
-        {
-            throw new NotImplementedException();
-        }
-        public void Name(string firstName, string middleName, string LastName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Post(string postName)
-        {
-            
-        }
-        
->>>>>>> 274242ba91cd921fafa476575da9e718da55e9e8
     }
 }
